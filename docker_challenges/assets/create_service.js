8bd8c99a873c070a98b2a9bbcba67eeb482f7db9
Ben Lucas <ben.lucas@choicehotels.com>
2025-12-09 18:34:01 -0700

    fix(ui): use window.CTFd to access global from ES6 modules
    ES6 modules execute in their own scope and cannot directly access
    global variables. Changed CTFd.plugin.run to window.CTFd.plugin.run
    in all challenge form JavaScript files to properly access the CTFd
    global object defined by the core framework.
    
    Fixes ReferenceError: CTFd is not defined at module initialization.
    
    Files updated:
    - create.js
    - create_service.js
    - update.js
    - update_service.js

--- docker_challenges/assets/create_service.js
+++ docker_challenges/assets/create_service.js
@@ -5,7 +5,7 @@ import {
     setupPortValidation,
 } from './shared/portManagement.js';
 
-CTFd.plugin.run((_CTFd) => {
+window.CTFd.plugin.run((_CTFd) => {
     const $ = _CTFd.lib.$;
     const md = _CTFd.lib.markdown();
     $('a[href="#new-desc-preview"]').on('shown.bs.tab', function (event) {
